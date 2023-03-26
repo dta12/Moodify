69f29b1220aa2a934644135a4a1d83e94ed121d4
April <amr272@pitt.edu>
2023-03-26 17:24:58 -0400

    improved UI design

--- moodify_app/lib/Screens/EmotionScreen.dart
+++ moodify_app/lib/Screens/EmotionScreen.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
 
 class EmotionScreen extends StatefulWidget{
-  const EmotionScreen()
+  const EmotionScreen();
+
   @override
-  State<StatefulWidget> createState() {
-    return new EmotionScreenState();
-  }
+  State<StatefulWidget> createState() => EmotionScreenState();
 }
 
 class EmotionScreenState extends State<EmotionScreen>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: getBackgroundColor(),
-      body: getEmotion(),
-
-    )
+      appBar: AppBar(
+        
+      ),
+      //backgroundColor: getBackgroundColor(),
+      //body: getEmotion(),
+      backgroundColor:  Color.fromARGB(255, 184, 150, 199),
+      body: 
+      Text("WIP"),
+    );
   }
+}
